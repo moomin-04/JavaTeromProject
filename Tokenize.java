5e4be06ad78d78a4b67bdc07628f30ac4db0f7e4
moomin-04 <smk6270@gmail.com>
2019-06-01 00:06:14 +0900

    Tokenize

--- Tokenize.java
+++ Tokenize.java
@@ -1,46 +1,63 @@
+package Test;
+
 import java.util.*;
+
 public class Tokenize {
-	private ArrayList<String> res=new ArrayList<String>();
-	
+	private ArrayList<String> res = new ArrayList<String>();
 	public ArrayList<String> getRes() {return res;}
 	
 	public Tokenize(StringBuffer sb) {
 		erase_comment(sb);
 		tokenizer(sb);
 	}
-	//Stack.h를 tokenize하기 이전에 주석을 지워주는 메소드
-	public void erase_comment(StringBuffer s){
-		int n=0,k=0;
+	
+	//주석을 지워주는 메소드
+	public void erase_comment(StringBuffer sb){
+		int n = 0, k = 0;
 		
-		while(k!=-1) {			
-			k=s.indexOf("//",k+1);//'//'의 위치 인덱스를  k에 저장
-			n=s.indexOf("\n",k);// '//'이후에 처음 나오는 '\n'의 위치 인덱스를 n에 저장
-			s.delete(k,n);// "//"과 "\n"사이를 delete해줌
+		while(true) {			
+			k = sb.indexOf("//",k+1);//'//'의 위치 인덱스를  k에 저장
+			if(k == -1) break;
+			n = sb.indexOf("\n",k);// '//'이후에 처음 나오는 '\n'의 위치 인덱스를 n에 저장
+			n++; //\n까지만 지웠을 땐 다음 문자 전에 \n가 적용돼서 나왔었음
+			sb.delete(k,n);// "//"과 "\n"사이를 delete해줌
 		}
 	}
-	//공백기준으로 StringBuffer를 나눈 후 {,(,),},;등도 모두 토큰화해줌
-	public ArrayList<String> tokenizer(StringBuffer sb){
+	
+	//주석을 제거한 StringBuffer를 기호에 따라 토큰화
+	public void tokenizer(StringBuffer sb){
 		String s=sb.toString();
 		
-		//공백문자를 기준으로 tokenizer
-		StringTokenizer st=new StringTokenizer(s);
-		for(int i=0; i<st.countTokens();i++) {
-			res.add(st.nextToken());//나뉜 token을 ArrayList로 저장
+		//구별기호 :{}();=
+		StringTokenizer st = new StringTokenizer(s, ":{}();= ", true) ;
+		
+		while(st.hasMoreTokens()) {
+			res.add(st.nextToken());
 		}
 		
-		//공백 이외 모두 토큰화
-		Iterator<String> res_it=res.iterator();
-		while(res_it.hasNext()) {
-			switch(res_it.next()) {
-			case()
+		//::, whitespace 처리
+		for(int i = 0; i < res.size(); i++) {
+			if(res.get(i).equals(":")) {
+				i++;
+				if(res.get(i).equals(":")) {
+					res.remove(i);
+					i--;
+					res.set(i, "::");
+				}
+			}
+			if(res.get(i).equals(" ")) {
+				res.remove(i);
 			}
 		}
-		return res;
-		
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		StringBuffer sb = new StringBuffer("Stack::Stack(int s:s){body}; //comment\nsize = 10;");
+		Tokenize t = new Tokenize(sb);
+		for(int i = 0; i < t.getRes().size(); i++) {
+			System.out.println("Token[" + i + "] : " + t.getRes().get(i));
+		}
 	}
 
 }
